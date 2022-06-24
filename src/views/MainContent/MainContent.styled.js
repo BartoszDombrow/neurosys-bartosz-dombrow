03a8a5167b33c75e7b88217353b187b37b38092a
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-06-24 19:36:01 +0200

    s

--- src/views/MainContent/MainContent.styled.js
+++ src/views/MainContent/MainContent.styled.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
 export const MainWrapper = styled.div`
+  display: flex;
   position: fixed;
   height: 700px;
   width: 1200px;
   background-color: rgba(250, 250, 255, 0.7);
-  padding: 20px 35px;
-  border-radius: 15px;
 `;
 
 export const Content = styled.div`
