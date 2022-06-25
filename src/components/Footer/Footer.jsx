96e56bda4f93f5e354de387b8778c9746567541e
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-06-25 14:48:22 +0200

    create footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
-import { Content, Button } from './Footer.styled';
+import {
+  Content,
+  Button,
+  FooterMenu,
+  FooterItem,
+  FooterIcon,
+  FooterText,
+  FooterLink,
+} from './Footer.styled';
+import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineYoutube } from 'react-icons/ai';
 
 function Footer() {
+  const footerItems = [
+    {
+      icon: <AiOutlineFacebook />,
+      text: 'Facebook',
+      link: 'https://www.facebook.com/oki47',
+    },
+    {
+      icon: <AiOutlineInstagram />,
+      text: 'Instagram',
+      link: 'https://www.instagram.com/spietadresiara/',
+    },
+    {
+      icon: <AiOutlineYoutube />,
+      text: 'YouTube',
+      link: 'https://www.youtube.com/watch?v=mQ7h1ujKiG4',
+    },
+  ];
   return (
     <Content>
-      <Button>elo</Button>
+      <FooterMenu>
+        {footerItems.map((footerItem) => {
+          return (
+            <FooterItem>
+              <FooterLink href={footerItem.link} target="_blank">
+                <FooterIcon>{footerItem.icon}</FooterIcon>
+                <FooterText>{footerItem.text}</FooterText>
+              </FooterLink>
+            </FooterItem>
+          );
+        })}
+        <Button>Kontakt</Button>
+      </FooterMenu>
     </Content>
   );
 }
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -10,5 +10,46 @@ export const Content = styled.div`
 `;
 
 export const Button = styled.button`
-  z-index: 100000000;
+  background-color: transparent;
+  justify-self: flex-end;
+  margin-left: 40px;
+  padding: 5px 10px;
+  border: 1px solid #fafaff;
+  font-size: 36px;
+  color: #fafaff;
+  cursor: pointer;
 `;
+
+export const FooterMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 100%;
+`;
+
+export const FooterItem = styled.li`
+  list-style: none;
+  font-size: 24px;
+  color: #fafaff;
+  cursor: pointer;
+`;
+export const FooterIcon = styled.div`
+  font-size: 28px;
+  padding: 0px 10px;
+`;
+
+export const FooterLink = styled.a`
+  display: flex;
+  text-decoration: none;
+  border-bottom: 1px solid transparent;
+  transition: 0.3s ease-in-out;
+  &:visited {
+    color: #fafaff;
+  }
+  &:hover {
+    border-bottom: 1px solid #fafaff;
+  }
+`;
+
+export const FooterText = styled.p``;
